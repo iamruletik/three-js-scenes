bbf7bb4f1eceee8f189ade0f5cc2453eb0a6594c
Ruletik <iam@ruletik.org>
2025-03-26 12:15:24 +0500

    Added Base

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,7 @@ import { defineConfig } from 'vite'
 const __dirname = dirname(fileURLToPath(import.meta.url))
 
 export default defineConfig({
+    base: '',
     server:
     {
         host: true, // Open to local network and display URL
