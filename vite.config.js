b6925e04da2ec0918205d29af5fa8f7300a93e47
Ruletik <iam@ruletik.org>
2025-03-28 11:53:37 +0500

    Reinstalled Vite and other packages, new npm init

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,8 @@
-import { dirname, resolve } from 'node:path'
-import { fileURLToPath } from 'node:url'
-import { defineConfig } from 'vite'
+import restart from 'vite-plugin-restart'
 
-const __dirname = dirname(fileURLToPath(import.meta.url))
-
-export default defineConfig({
-    base: '',
+export default {
+    //root: '', // Sources files (typically where index.html is)
+    //publicDir: '../static/', // Path from "root" to static assets (files that are served as they are)
     server:
     {
         host: true, // Open to local network and display URL
@@ -13,15 +10,12 @@ export default defineConfig({
     },
     build:
     {
-        outDir: '../build/', // Output in the dist/ folder
+        outDir: '../dist', // Output in the dist/ folder
         emptyOutDir: true, // Empty the folder first
-        sourcemap: true, // Add sourcemap
-        rollupOptions: {
-            input: {
-              main: resolve(__dirname, 'index.html'),
-              house: resolve(__dirname, '/hauntedhouse/index.html'),
-            },
-            external: ['three', 'gsap', 'tweakpane'],
-          },
+        sourcemap: true // Add sourcemap
     },
-})
+    plugins:
+    [
+        restart({ restart: [ '../static/**', ] }) // Restart server on static file change
+    ],
+}
