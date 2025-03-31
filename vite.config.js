35a3d1d2812569cecf0663f6476d4e0096dd4c36
Ruletik <iam@ruletik.org>
2025-03-31 17:06:41 +0500

    Multi Page Vite Config

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,9 @@
 import restart from 'vite-plugin-restart'
+import { dirname, resolve } from 'node:path'
+import { fileURLToPath } from 'node:url'
+import { defineConfig } from 'vite'
+
+const __dirname = dirname(fileURLToPath(import.meta.url))
 
 export default {
     //root: '', // Sources files (typically where index.html is)
@@ -12,7 +17,14 @@ export default {
     {
         outDir: 'dist/', // Output in the dist/ folder
         emptyOutDir: true, // Empty the folder first
-        sourcemap: true // Add sourcemap
+        sourcemap: true, // Add sourcemap
+        rollupOptions: {
+            input: {
+              main: resolve(__dirname, 'index.html'),
+              hauntedhouse: resolve(__dirname, '/hauntedhouse/index.html'),
+              particlestest: resolve(__dirname, '/particlestest/index.html'),
+            },
+        }
     },
     plugins:
     [
